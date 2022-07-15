be063ac030668999e62d5ff7b0717fa838851f19
Tokhir <karamatov.tohirbek@gmail.com>
2022-07-15 23:51:43 +0300

    video launch

---
+++ script.js
@@ -0,0 +1,12 @@
+const video = document.getElementById('video');
+
+
+function startVideo() {
+  navigator.getUserMedia(
+    {video: {}},
+    stream => video.srcObject = stream,
+    error => console.error(error)
+  )
+}
+
+startVideo();
